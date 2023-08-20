e1ee6f2848696a7e3bc4fd74d60ee9df4a135f16
SOUMOJIT MANDAL <soumojit.jit@gmail.com>
2023-08-20 22:53:18 +0530

    recursion initial practice code printing numbers from 1 to 20

---
+++ Recursion/printnum_basic.java
@@ -0,0 +1,20 @@
+package com.Mindtree.Recursion;
+//recursion is when the function calls itself multiple times until an exception is provided for eg, a return statement
+// as many times we call the same function it will consume the memory each time we call
+// recursion helps us in solving bigger and complex problems easily (Dynamic programming )
+// convert recursion solution into iteration and vice versa
+//Space complexity is not constant because of recursive calls
+//break down bigger problems into smaller ones
+public class printnum_basic {
+    public static void main(String[] args) {
+        print(1);
+    }
+    static void print(int n){
+        if (n==20){                 //base condition in recursion after which the recursion will stop making further calls
+            System.out.println(n);
+            return;
+        }
+        System.out.println(n);
+        print(n+1);
+    }
+}
