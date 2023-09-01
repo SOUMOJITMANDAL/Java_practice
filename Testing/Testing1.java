1019bdc25746d8ff84da8d7429ad5b9a13284a83
SOUMOJIT MANDAL <soumojit.jit@gmail.com>
2023-09-01 11:39:33 +0530

    code revised

--- Testing/Testing1.java
+++ Testing/Testing1.java
@@ -1,4 +1,14 @@
 package com.Mindtree.Testing;
-
+import org.junit.Assert;
+import org.junit.Test;
 public class Testing1 {
+    String message = "SoumojitTheTester";
+
+    @Test
+    public void testPrintMessage(){
+        System.out.println("Inside testPrintMessage() ");
+        Assert.assertEquals("SoumojitTheTester",message);
+    }
 }
+
+
