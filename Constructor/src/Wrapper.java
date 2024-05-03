97e8d222f9579ac2fcfc613f2123bfe9198cc205
SoumojitMandal <soumojit.jit@gmail.com>
2024-05-03 16:29:19 +0530

    Wrapper Class demo

---
+++ Constructor/src/Wrapper.java
@@ -0,0 +1,10 @@
+public class Wrapper {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        Integer num1 = 100;
+        Integer num2 = 200;
+
+    }
+}
